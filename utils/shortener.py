e5313c2430aa96e96193b777f6af9624b05e11a1
watson <watson69@qq.com>
2021-01-30 13:02:02 +0800

    增加短链功能 1. 转换URL放到body里面请求

--- utils/shortener.py
+++ utils/shortener.py
@@ -39,7 +39,10 @@ class Shortener(object):
         header = {"x-auth-short-app": self.app,
                   "x-auth-short-sign": signature,
                   "Content-Type": "application/x-www-form-urlencoded"}
-        response = requests.request(method="POST", url="%s?%s" % (self.app_url, payload), headers=header)
+        data = {
+            'longUrl': url
+        }
+        response = requests.request(method="POST", url=self.app_url, headers=header, data=data)
         if response.ok:
             result = response.json()
             logger.info(dict(app='shortener', msg=result))
